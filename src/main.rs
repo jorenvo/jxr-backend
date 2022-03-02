bdb52687d340736b315b4ec780e3d63a71e36c43
Joren Van Onder <joren@jvo.sh>
2022-03-01 17:46:49 -0800

    Extract some parameters

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::process::Command;
 
-fn run_ripgrep(pattern: &str) -> String {
-    let output = Command::new("rg")
-        .arg("--json")
-        .arg(pattern)
-        .arg("/home/jvo/odoo_versions/odoo_v14")
-        .arg("/home/jvo/odoo_versions/enterprise_v14")
-        .output()
-        .expect("failed to execute process");
-    return String::from_utf8(output.stdout).expect("ripgrep output is not utf8");
+fn run_ripgrep(paths: &[&str], pattern: &str) -> String {
+    let mut command = Command::new("rg");
+
+    command.arg("--json");
+    command.arg(pattern);
+
+    for path in paths {
+        command.arg(path);
+    }
+
+    let output = command.output().expect("failed to execute process");
+    String::from_utf8(output.stdout).expect("ripgrep output is not utf8")
 }
 
 fn main() {
     println!("Hello, world!");
-
-    println!("{}", run_ripgrep("Please add your AvaTax credentials"));
+    println!(
+        "{}",
+        run_ripgrep(
+            &["/Users/jvo/Code/jxr-backend", "/Users/jvo/Code/gb-emu"],
+            "ripgrep output is not utf8"
+        )
+    );
 }
